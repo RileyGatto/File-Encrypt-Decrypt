a3b4ea3a3ed10c45a6fb75adb27cee3a06d994b8
Riley Gatto <rmgatto64@gmail.com>
2025-12-18 12:56:56 +1100

    Main menu visual created

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    
+    int choice;
+
+    cout << "\nWelcome To My File Converter\n" << endl;
+    cout << "==============================" << endl;
+    cout << "         Main Menu            " << endl;
+    cout << "==============================" << endl;
+    cout << "1. Encrypt File" << endl;
+    cout << "2. Decrypt File" << endl;
+    cout << "3. Exit\n" << endl;
+    cout << "Your choice: ";
+    cin >> choice;
+    
+    return 0;
+}
